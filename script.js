cdd0434cfd6d51d4742af585c2c58ceb7ed5ad2b
Muhammaddavron <muhammaddavron123@gmail.com>
2024-08-03 10:49:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let ism = prompt(`strind kiriting`);
+let ism_familiya = prompt(`number kiriting`);
+let maktab = prompt(`boolean kiriting`);
+let manzil = prompt(`null kiriting`);
+let yil_sana = prompt(`undefined kiriting`);
+let marsdagi_ustoz = prompt(`symbol  kiriting`);
+
+console.log(ism)
+console.log(ism_familiya)
+console.log(maktab)
+console.log(manzil)
+console.log(yil_sana)
+console.log(marsdagi_ustoz)
